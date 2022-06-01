35fda34665af84af3ff84c7a79a0030290894f25
João Antunes <joaoantunes20102010@hotmail.com>
2022-06-01 18:32:39 +0100

    Commit

--- SIte/scripts/index.js
+++ SIte/scripts/index.js
@@ -1,4 +1,4 @@
-/* MENU SHOW */ 
+
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -13,22 +13,16 @@ const showMenu = (toggleId, navId) =>{
 showMenu('nav-toggle','nav-menu')
 
 
-// OVERLAY
-gsap.to(".primeiro", 1.5, {delay: .5, top: "-100%", ease: Expo.easeInOut});
-gsap.to(".segundo", 1.5, {delay: .7, top: "-100%", ease: Expo.easeInOut});
-gsap.to(".terceiro", 1.5, {delay: .9, top: "-100%", ease: Expo.easeInOut});
+gsap.to(".primeiro", 2.3, {delay: .7, top: "-100%", ease: Expo.easeInOut});
+gsap.to(".segundo", 2.3, {delay: 1.3, top: "-100%", ease: Expo.easeInOut});
+gsap.to(".terceiro", 2.3, {delay: 1.8, top: "-100%", ease: Expo.easeInOut});
 
 
-// IMG
-gsap.from('.home__img', {opacity: 0, duration: 2, delay: 2, x: 60})
 
-// INFORMATION
-gsap.from('.home__information', {opacity: 0, duration: 3, delay: 2.3, y: 25})
-gsap.from('.anime-text', {opacity: 0, duration: 3, delay: 2.3, y: 25, ease:'expo.out', stagger: .3})
-
-// NAV ITEM
-gsap.from('.nav__logo', {opacity:0, duration: 3, delay: 3.2, y: 25, ease:'expo.out'});
-gsap.from('.nav__item', {opacity: 0, duration: 3, delay: 3.2, y: 25, ease:'expo.out', stagger: .2})
-
-// SOCIAL
-gsap.from('.home__social-icon', {opacity: 0, duration: 3, delay: 4, y: 25, ease:'expo.out', stagger: .2})
+gsap.from('.home__img', {opacity: 0, duration: 5, delay: 3, x: 60})
+gsap.from('.home__information', {opacity: 0, duration: 9, delay: 4, y: 25})
+gsap.from('.anime-text', {opacity: 0, duration: 9, delay: 5, y: 25, ease:'expo.out', stagger: .3})
+gsap.from('.anime-text-1', {opacity: 0, duration: 15, delay: 3, y: 25, ease:'expo.out', stagger: .3})
+gsap.from('.nav__logo', {opacity:0, duration: 3, delay: 6, y: 25, ease:'expo.out'});
+gsap.from('.nav__item', {opacity: 0, duration: 3, delay: 6.9, y: 25, ease:'expo.out', stagger: .2})
+gsap.from('.home__social-icon', {opacity: 0, duration: 3, delay: 7.3, y: 25, ease:'expo.out', stagger: .2})
