5f3d80e2d0da0891b40eabb43d24eaf52533449c
João Antunes <joaoantunes20102010@hotmail.com>
2022-06-05 17:57:01 +0100

    Commit

--- SIte/scripts/index.js
+++ SIte/scripts/index.js
@@ -1,7 +1,13 @@
 
+$("#hamburguer").click( function() {
+	$(".icon").toggleClass("close");
+});
+
+
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
+
 
     if(toggle && nav){
         toggle.addEventListener('click', ()=>{
@@ -10,7 +16,9 @@ const showMenu = (toggleId, navId) =>{
     }
 }
 
+
 showMenu('nav-toggle','nav-menu')
+
 
 
 gsap.to(".primeiro", 2.3, {delay: .7, top: "-100%", ease: Expo.easeInOut});
@@ -20,9 +28,9 @@ gsap.to(".terceiro", 2.3, {delay: 1.8, top: "-100%", ease: Expo.easeInOut});
 
 
 gsap.from('.home__img', {opacity: 0, duration: 5, delay: 3, x: 60})
-gsap.from('.home__information', {opacity: 0, duration: 9, delay: 4, y: 25})
+gsap.from('.home__info', {opacity: 0, duration: 9, delay: 4.5, y: 25})
 gsap.from('.anime-text', {opacity: 0, duration: 9, delay: 5, y: 25, ease:'expo.out', stagger: .3})
-gsap.from('.anime-text-1', {opacity: 0, duration: 15, delay: 3, y: 25, ease:'expo.out', stagger: .3})
+gsap.from('.anime-text-1', {opacity: 0, duration: 9, delay: 3, y: 25, ease:'expo.out', stagger: .3})
 gsap.from('.nav__logo', {opacity:0, duration: 3, delay: 6, y: 25, ease:'expo.out'});
 gsap.from('.nav__item', {opacity: 0, duration: 3, delay: 6.9, y: 25, ease:'expo.out', stagger: .2})
 gsap.from('.home__social-icon', {opacity: 0, duration: 3, delay: 7.3, y: 25, ease:'expo.out', stagger: .2})
